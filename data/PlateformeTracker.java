6a1cdb10a8ad6b3292345df543986fba2a06b8c0
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-07-08 12:34:34 +0200

    (feat) connection to database

--- data/PlateformeTracker.java
+++ data/PlateformeTracker.java
@@ -1,47 +1,47 @@
 import java.sql.*;
+import java.util.Vector;
 
 public class PlateformeTracker {
 
-        public void initDataBase()
-        {
-            Connection connection = null;
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
+    public void initDataBase() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-                // Lucas                
-                // connection = DriverManager.getConnection(
-                //     "jdbc:mysql://localhost:3306/mydb",
-                //     "root", "$~Bc4gB9");
-    
-                // Vanny
-                connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/mydb",
-                "root", "VannyLamorte25!");	
-                    
-                // Thanh
-                // connection = DriverManager.getConnection(
-                //     "jdbc:mysql://localhost:3306/mydb",
-                //     "root", "root");
-    
-                Statement statement;
-                statement = connection.createStatement();
-                ResultSet resultSet;
-                resultSet = statement.executeQuery(
-                    "select * from student");
-                int code;
-                String title;
-                while (resultSet.next()) {
-                    code = resultSet.getInt("code");
-                    title = resultSet.getString("title").trim();
-                    System.out.println("Code : " + code
-                                    + " Title : " + title);
+            Vector<String> passwords = new Vector<>();
+            passwords.add("VannyLamorte25!"); // --- Vanny
+            passwords.add("root"); // --- Thanh
+            passwords.add("$~Bc4gB9"); // --- Lucas
+
+            for (String password : passwords) {
+                try {
+                    connection = DriverManager.getConnection(
+                            "jdbc:mysql://localhost:3306/PlateformeTracker",
+                            "root", password);
+                            break;
+
+                } catch (Exception exception) {
                 }
-                resultSet.close();
-                statement.close();
-                connection.close();
             }
-            catch (Exception exception) {
-                System.out.println(exception);
+
+            Statement statement;
+            statement = connection.createStatement();
+            ResultSet resultSet;
+            resultSet = statement.executeQuery(
+                    "select * from student");
+            String firstName;
+            int age;
+            while (resultSet.next()) {
+                firstName = resultSet.getString("firstName");
+                age = resultSet.getInt("age");
+                System.out.println("First name : " + firstName
+                        + " | Age : " + age);
             }
-        } 
-    } 
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+    }
+}
