93363d6517691591658825b74b7af548d63c8be5
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-07-09 09:27:03 +0200

    (feat) add methods to check is the user input is valid

---
+++ src/inputValidator.java
@@ -0,0 +1,45 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class inputValidator {
+
+    public class inputUSer {
+
+        //--- MENU ---//
+
+        // Validate only numbers between 1 and 5
+        public static boolean isValidMenu(String input) {
+            return input.matches("[1-5]");
+        } 
+
+        //--- UPDATE STUDENT INFO ---//
+
+        // Validate only digits
+        public static boolean isDigit(String input) {
+            return input.matches("\\d+");
+        }
+        
+        // Validate only letters
+        public static boolean isAlphabetic(String input) {
+            return input.matches("[a-zA-Z]+"); 
+        }
+        
+        // Validate only Yes, yes, Y, No, no or N
+        public static boolean isValidYesNo(String input) {
+            Set<String> validInputs = new HashSet<>();
+            validInputs.add("Yes");
+            validInputs.add("yes");
+            validInputs.add("Y");
+            validInputs.add("No");
+            validInputs.add("no");
+            validInputs.add("N");
+            return validInputs.contains(input);
+        }
+
+        // Validate only numbers between 1 and 5
+        public static boolean isValidUpdateStudentInfo(String input) {
+        return input.matches("[1-5]");
+        }       
+    }
+    
+}
