3e2238229355a116d5812104f8ccd0951a8bf1c2
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-07-18 14:16:15 +0200

    (fixed) login method

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,13 @@ public class Main {
 
         try (Scanner input = new Scanner(System.in)) {
             HomeDisplay display = new HomeDisplay(input);
-            LoginDisplay login = new LoginDisplay(input);
             StudentRepository studentRepo = new StudentRepository();
             // Start the AutoBackup in a separate thread
             AutoBackup autoBackup = new AutoBackup(studentRepo);
             Thread backupThread = new Thread(() -> autoBackup.startBackupSchedule(60)); // Backup every 60 seconds
             backupThread.setDaemon(true); // Ensure it doesn't block JVM shutdown
             backupThread.start();
-            if (login.userAccount())
-                display.homeDisplay();
+            display.homeDisplay();
         }
     }
 }
